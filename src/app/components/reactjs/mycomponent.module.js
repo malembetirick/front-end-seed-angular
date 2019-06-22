001a7829d0fd7f297e4d130a854ebdef18ff2a35
malembetirick <malembetirick@gmail.com>
2019-06-22 22:34:06 +0200

    test update component from view done

--- src/app/components/reactjs/mycomponent.module.js
+++ src/app/components/reactjs/mycomponent.module.js
@@ -1,4 +1,4 @@
 import { react2angular } from 'react2angular'
 import mycomponent from "../reactjs/mycomponent"
 angular.module('reactEmbed', [])
-    .component("myComponent", react2angular(mycomponent));
+    .component("myComponent", react2angular(mycomponent, ['fooBar', 'baz']));
